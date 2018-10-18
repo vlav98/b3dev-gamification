acb2b1b2f599c864b9aac0441b615c88796d6132
vlav98 <32904482+vlav98@users.noreply.github.com>
2018-10-18 10:35:22 +0200

    Add files via upload

--- projet_b3dev.sql
+++ projet_b3dev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 12 Octobre 2018 à 11:13
+-- Généré le :  Jeu 18 Octobre 2018 à 08:35
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -28,6 +28,7 @@ USE `projet_b3dev`;
 -- Structure de la table `article`
 --
 
+DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article` (
   `article_id` int(11) NOT NULL,
   `article_name` varchar(25) NOT NULL,
@@ -36,7 +37,8 @@ CREATE TABLE `article` (
   `article_price` double NOT NULL,
   `article_shippingprice` double NOT NULL,
   `article_shippingdelay` int(11) NOT NULL,
-  `article_category` int(11) NOT NULL
+  `article_category` int(11) NOT NULL,
+  `article_stock` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -45,6 +47,7 @@ CREATE TABLE `article` (
 -- Structure de la table `category`
 --
 
+DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(25) NOT NULL
@@ -56,6 +59,7 @@ CREATE TABLE `category` (
 -- Structure de la table `coupons`
 --
 
+DROP TABLE IF EXISTS `coupons`;
 CREATE TABLE `coupons` (
   `coupon_id` int(11) NOT NULL,
   `coupon_value` double NOT NULL,
@@ -65,9 +69,24 @@ CREATE TABLE `coupons` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `rules`
+--
+
+DROP TABLE IF EXISTS `rules`;
+CREATE TABLE `rules` (
+  `rules_id` int(11) NOT NULL,
+  `rules_label` varchar(50) NOT NULL,
+  `condition` varchar(50) NOT NULL,
+  `target` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `shop`
 --
 
+DROP TABLE IF EXISTS `shop`;
 CREATE TABLE `shop` (
   `shop_id` int(11) NOT NULL,
   `shop_name` varchar(25) NOT NULL,
@@ -80,17 +99,39 @@ CREATE TABLE `shop` (
 -- Structure de la table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
-  `user_type` enum('admin','client','seller') NOT NULL,
+  `user_type` int(11) NOT NULL,
   `username` varchar(25) NOT NULL,
   `user_email` varchar(50) NOT NULL,
   `firstname` text NOT NULL,
   `lastname` text NOT NULL,
   `password` varchar(25) NOT NULL,
   `points` int(11) NOT NULL,
-  `statut` enum('bronze','argent','or','platine','diamant') NOT NULL
+  `statut` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_type`
+--
+
+DROP TABLE IF EXISTS `user_type`;
+CREATE TABLE `user_type` (
+  `id_type` int(11) NOT NULL,
+  `type_name` varchar(25) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `user_type`
+--
+
+INSERT INTO `user_type` (`id_type`, `type_name`) VALUES
+(1, 'admin'),
+(2, 'seller'),
+(3, 'client');
 
 --
 -- Index pour les tables exportées
@@ -115,6 +156,12 @@ ALTER TABLE `coupons`
   ADD PRIMARY KEY (`coupon_id`);
 
 --
+-- Index pour la table `rules`
+--
+ALTER TABLE `rules`
+  ADD PRIMARY KEY (`rules_id`);
+
+--
 -- Index pour la table `shop`
 --
 ALTER TABLE `shop`
@@ -125,6 +172,12 @@ ALTER TABLE `shop`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- Index pour la table `user_type`
+--
+ALTER TABLE `user_type`
+  ADD PRIMARY KEY (`id_type`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -146,6 +199,11 @@ ALTER TABLE `category`
 ALTER TABLE `coupons`
   MODIFY `coupon_id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT pour la table `rules`
+--
+ALTER TABLE `rules`
+  MODIFY `rules_id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT pour la table `shop`
 --
 ALTER TABLE `shop`
@@ -155,6 +213,11 @@ ALTER TABLE `shop`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `user_type`
+--
+ALTER TABLE `user_type`
+  MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
